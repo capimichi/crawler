004f12d03a4e268681982710dfb25ab6927de6fc
Michele Capicchioni <capimichi@gmail.com>
2017-07-11 23:34:00 +0200

    [dev] Test working

---
+++ test/Crawler/Downloader/PhantomDownloaderTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michelecapicchioni
+ * Date: 11/07/17
+ * Time: 17:56
+ */
+
+namespace Crawler\Test\Downloader;
+
+
+use Crawler\Downloader\PhantomDownloader;
+use PHPUnit\Framework\TestCase;
+
+class PhantomDownloaderTest extends TestCase
+{
+
+    /**
+     * Test if downloader is working
+     */
+    public function testCanDownload()
+    {
+        $phantomDownloader = new PhantomDownloader("http://www.google.com");
+        $content = $phantomDownloader->getContent();
+        self::assertNotEmpty($content);
+    }
+
+}
