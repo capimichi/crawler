ef4c4effac6b4fbc3fd0a0c0b0290e0f2b38349c
Michele Capicchioni <capimichi@gmail.com>
2017-07-13 20:23:49 +0200

    [dev] User agent selection in phantomDownloader

--- src/Crawler/Downloader/PhantomDownloader.php
+++ src/Crawler/Downloader/PhantomDownloader.php
@@ -10,20 +10,29 @@ namespace Crawler\Downloader;
 
 
 use JonnyW\PhantomJs\Client;
+use JonnyW\PhantomJs\Http\Request;
 
 class PhantomDownloader extends Downloader
 {
 
     const PHANTOMJS_PATH = __DIR__ . "/../../../bin/phantomjs";
 
+    const DEFAULT_USERAGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
+
     /**
      * @var string
      */
     protected $phantomjsPath;
 
+    /**
+     * @var string
+     */
+    protected $userAgent;
+
     public function __construct($url)
     {
         $this->setPhantomjsPath(realpath(self::PHANTOMJS_PATH));
+        $this->setUserAgent(self::DEFAULT_USERAGENT);
 
         parent::__construct($url);
     }
@@ -37,7 +46,11 @@ class PhantomDownloader extends Downloader
 
         $client->getEngine()->setPath($this->getPhantomjsPath());
 
-        $request = $client->getMessageFactory()->createRequest($this->url, 'GET');
+//        $request = $client->getMessageFactory()->createRequest($this->url, 'GET');
+
+        $request = new Request($this->url, 'GET');
+
+        $request->addSetting('userAgent', $this->getUserAgent());
 
         $response = $client->getMessageFactory()->createResponse();
 
@@ -81,6 +94,22 @@ class PhantomDownloader extends Downloader
     public function setPhantomjsPath($phantomjsPath)
     {
         $this->phantomjsPath = $phantomjsPath;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserAgent()
+    {
+        return $this->userAgent;
+    }
+
+    /**
+     * @param string $userAgent
+     */
+    public function setUserAgent($userAgent)
+    {
+        $this->userAgent = $userAgent;
     }
 
 
